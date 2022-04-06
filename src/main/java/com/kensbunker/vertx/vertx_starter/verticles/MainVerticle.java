37fbe49c61375922185fcb524e2a3d6a4231569a
kmandawe <kdmandawe06@gmail.com>
2022-04-06 14:32:14 +0800

    Vert.x Verticle Config

--- src/main/java/com/kensbunker/vertx/vertx_starter/verticles/MainVerticle.java
+++ src/main/java/com/kensbunker/vertx/vertx_starter/verticles/MainVerticle.java
@@ -4,6 +4,9 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+
+import java.util.UUID;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -17,7 +20,14 @@ public class MainVerticle extends AbstractVerticle {
     System.out.println("Start " + getClass().getName());
     vertx.deployVerticle(new VerticleA());
     vertx.deployVerticle(new VerticleB());
-    vertx.deployVerticle(VerticleN.class.getName(), new DeploymentOptions().setInstances(4));
+    vertx.deployVerticle(
+        VerticleN.class.getName(),
+        new DeploymentOptions()
+            .setInstances(4)
+            .setConfig(
+                new JsonObject()
+                    .put("id", UUID.randomUUID().toString())
+                    .put("name", VerticleN.class.getSimpleName())));
     startPromise.complete();
   }
 }
--- src/main/java/com/kensbunker/vertx/vertx_starter/verticles/VerticleN.java
+++ src/main/java/com/kensbunker/vertx/vertx_starter/verticles/VerticleN.java
@@ -7,7 +7,7 @@ public class VerticleN extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    System.out.println("Start " + getClass().getName() + " on thread " + Thread.currentThread().getName());
+    System.out.println("Start " + getClass().getName() + " with config " + config().toString());
     startPromise.complete();
   }
 }
