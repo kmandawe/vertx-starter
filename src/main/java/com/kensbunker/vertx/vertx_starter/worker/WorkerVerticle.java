6c7c775039b90594ffb814b9749cbe4194348bfc
kmandawe <kdmandawe06@gmail.com>
2022-04-06 16:31:07 +0800

    Worker Threads

---
+++ src/main/java/com/kensbunker/vertx/vertx_starter/worker/WorkerVerticle.java
@@ -0,0 +1,19 @@
+package com.kensbunker.vertx.vertx_starter.worker;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WorkerVerticle extends AbstractVerticle {
+
+  private static final Logger LOG = LoggerFactory.getLogger(WorkerVerticle.class);
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    LOG.debug("Deployed as worker verticle.");
+    startPromise.complete();
+    Thread.sleep(5000);
+    LOG.debug("Blocking operation done.");
+  }
+}
